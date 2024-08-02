0b327c83e2094e5c48b483d964cbeb06f9bfb9d5
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-02 22:59:36 +0530

    Beautification

--- components/bottom_bar_component_1722619731.js
+++ components/bottom_bar_component_1722619731.js
@@ -3,11 +3,11 @@ It has two buttons: Contact button that directs to the contact page and Pay butt
 */
 Vue.component("bottom_bar_component_1722619731", {
     template: `
-    <section id="bottom-bar-component" class="fixed bottom-0 z-30 w-full lg:w-1/2 !bg-opacity-80 backdrop-blur-sm bg-white dark:bg-slate-950">
-        <div id="bottom-bar-component-content" class="w-full max-w-xl mx-auto px-4 sm:px-6">
-            <div id="action-buttons" class="flex py-4 md:py-6 space-x-4 justify-center">
-                <a id="contact-button" class="btn flex items-center justify-center w-1/2 h-12 p-3 text-slate-900 dark:text-slate-200 dark:bg-slate-900 rounded-lg hover:border-slate-300 dark:border-slate-800 dark:hover:border-slate-700 border shadow-lg transition duration-200 ease-in-out" href="contact.html">Contact</a>
-                <a id="pay-button" class="btn flex items-center justify-center w-1/2 h-12 p-3 animate-shine bg-[size:200%_100%] hover:bg-[image:none] transition duration-200 ease-in-out shadow-lg text-white bg-blue-500 hover:bg-blue-600 rounded-lg bg-[linear-gradient(100deg, theme(colors.blue.500), 45%, theme(colors.blue.400), 55%, theme(colors.blue.500))] rounded-lg">Pay</a>
+    <section id="bottom-bar-component" class="fixed bottom-0 z-30 w-full lg:w-1/2 bg-opacity-40 backdrop-blur-lg bg-pink-300 dark:bg-purple-900 rounded-lg">
+        <div id="bottom-bar-component-content" class="w-full max-w-xl mx-auto px-6 sm:px-8">
+            <div id="action-buttons" class="flex py-6 md:py-8 space-x-6 justify-center">
+                <a id="contact-button" class="btn flex items-center justify-center w-1/2 h-14 p-4 text-white bg-opacity-30 backdrop-filter backdrop-blur-lg bg-pink-500 dark:bg-purple-700 rounded-lg hover:bg-opacity-50 transition duration-300 ease-in-out shadow-lg" href="contact.html">Contact</a>
+                <a id="pay-button" class="btn flex items-center justify-center w-1/2 h-14 p-4 animate-shine bg-[size:200%_100%] hover:bg-opacity-70 transition duration-300 ease-in-out shadow-lg text-white bg-opacity-50 backdrop-filter backdrop-blur-lg bg-purple-600 dark:bg-pink-600 rounded-lg hover:bg-purple-700 dark:hover:bg-pink-700">Pay</a>
             </div>
         </div>
     </section>
